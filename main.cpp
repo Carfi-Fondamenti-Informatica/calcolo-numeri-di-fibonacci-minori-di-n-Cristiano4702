d70cdafbc13162180b72393d9af3a8a71d311f7d
Cristiano4702 <91879010+Cristiano4702@users.noreply.github.com>
2021-11-14 17:41:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 int main() {
-   int n, a=1, b=1, c;
-    cin >> n;
+   int n=0, a=1, b=1, c;
+   cin >> n;
 
-    cout << a << endl;
-    cout << b << endl;
+   cout << a << endl;
+   cout << b << endl;
 
     for(int i=2; i<n; i++)
     {
         c = a+b;
 
-        if(c<n)
-        {
-            cout << c << endl;
-        }
+         if(c<n)
+         {
+              cout << c << endl;
+         }
 
-        else
-        {
-            return 0;
-        }
+         else
+         {
+               return 0;
+         }
 
         a=b;
         b=c;
